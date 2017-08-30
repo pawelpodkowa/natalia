3e70542dd207219b19debb39ca1e253ad149887a
pawelpodkowa <ppodkowa@gmail.com>
2017-08-30 15:24:19 +0200

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <!--<![endif]-->
   <head>
       <!-- Meta tags -->
-      <meta charset="utf-8">
+        <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name='description' content=''>
@@ -21,7 +21,7 @@
 
   </head>
     <body>
- 
+
 
 
 
