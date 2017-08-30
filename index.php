fc21a46c04128313a9ffcd0bd2cf8d55a50a280d
pawelpodkowa <ppodkowa@gmail.com>
2017-08-30 14:09:04 +0200

    first look

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<!--[if IE 7]><html class="ie ie7 ltie8 ltie9" lang="pl-PL"><![endif]-->
+<!--[if IE 8]><html class="ie ie8 ltie9" lang="pl-PL"><![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html lang="pl-PL">
+<!--<![endif]-->
+  <head>
+      <!-- Meta tags -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name='description' content=''>
+      <meta name='keywords' content=''>
+      <title>Natalia Wojtecka - Personall Trainer</title>
+      <link rel="icon" href="favicon.ico" />
+      <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified JavaScript -->
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </head>
+    <body>
+      
+
+
+
+    </body>
+  </html>
